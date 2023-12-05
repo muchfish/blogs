9a88ed62cb1599ca49ed197cfff1c37ee27ade77
touchfishvirtuoso <152949892+touchfishvirtuoso@users.noreply.github.com>
2023-12-05 15:50:27 +0800

    Update config.ts
    测试

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blogs/",
 
   lang: "zh-CN",
   title: "萌萌哒草头将军",
