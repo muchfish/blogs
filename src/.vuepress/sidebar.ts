8071616283460b07ef8b32eb69eb4a3b70f59661
daiwencheng <daiwencheng@shengqugames.com>
2023-12-07 19:52:03 +0800

    精简博客

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,40 +1,6 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  // "/java/":  "structure",
+  "/java/":  "structure",
   "/posts/":  "structure",
-  // 必须放在最后面
-  "/": [
-    {
-      text: "博客首页",
-      icon: "star",
-      collapsible: true,
-      prefix: "/",
-      children: ["intro"],
-    },
-    {
-      text: "Java",
-      icon: "java",
-      collapsible: false,
-      prefix: "java/",
-      children: [
-        {
-          text: "基础",
-          prefix: "basis/",
-          icon: "basic",
-          children: [
-            "java-basic-questions-01",
-            {
-              text: "重要知识点",
-              icon: "star",
-              collapsible: true,
-              children: [
-                "why-there-only-value-passing-in-java",
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ],
 });
---
+++ src/java/ruoyipro.md
@@ -0,0 +1,11 @@
+---
+title: ruo-vue-pro笔记
+isTimeLine: true
+date: 2023-12-07
+category:
+  - Java
+tag:
+  - java
+---
+
+占位
